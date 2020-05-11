632bc84a632dd356c52bc74d93a1eb7ecc3945d0
liucms <liucms@users.noreply.github.com>
2020-05-11 15:52:53 +0800

    VCMS
    VCMS

---
+++ Runtime/Data/_nav/list.php
@@ -0,0 +1,6 @@
+<?php
+return array (
+  '网站广告管理' => 'index.php?s=Admin-Ads-Show',
+  '影片数据管理' => 'index.php?s=Admin-Vod-Show',
+);
+?>
---
+++ admin.php
@@ -0,0 +1,6 @@
+<?php
+@session_start();
+$_SESSION['AdminLogin'] = 1;
+$_SESSION['host_key'] = ''; // 后台登陆域名,只允许唯一该域名访问后台管理界面
+header("Location: ./index.php?s=Admin-Login"); 
+?>
